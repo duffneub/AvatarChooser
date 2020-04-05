f3a727a4bf37690c602370e76c7ba6e7682a93aa
Duff Neubauer <d.neubauer@me.com>
2020-04-05 13:58:41 -0700

    Initial Commit

---
+++ AvatarChooser/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  AvatarChooser
+//
+//  Created by Duff Neubauer on 4/5/20.
+//  Copyright © 2020 Duff Neubauer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
